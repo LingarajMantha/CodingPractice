d97a0f693742fbb6ef97d67248fee70fd02cd7ab
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-09-10 18:38:23 +0530

    Update Arrays_Theory_Problems.java

--- Arrays_Theory_Problems.java
+++ Arrays_Theory_Problems.java
@@ -16,15 +16,40 @@ arr[0]=10;  arr[1]=20,, similarly ,, what would you do for the 1000 elements to 
     int [] arr = new int [100];
   - for (int i=0; i,arr.length; i++){
     arr[i] = in.nextInt();
-  -for (int i=0; i,arr.length; i++){
+  -for (int i=0; i<arr.length; i++){
     System.out.println(arr[i] + "");
 }
 
     //using  fo each loop, this is usually used to preint each and every element in the array ,, 
-    for (int  num :arr){ // ehnanced for loop , for the every elemnt in array,, for each loop
-      System.out.println(num + " ");//here num represents element of the array
+    for (int  num :arr){       // ehnanced for loop , for the every elemnt in array,, for each loop
+      System.out.println(num + " ");          //here num represents element of the array
       - 
-  ============================================================================================
+
+  ----- toString();   -- toString method 
+        defn - Array class has toString() method , when you give array to it , basically it converts to string and prints that strings,
+        - 
+
+        ------ Array of objects ---------------
+      String[] str = new String[4];   //contin the 4 objects ,, it contains the ref. variable for the objects 
+      for (i=0; i<str.length; i++){
+        str[i]= in.next();
+      }
+      Syop(Arrays.toString(str));
+
+      ----------------Array of Objects -----------------------
+>>String [] str = new String [4]; // its storing the 4 objects not the 4 elemsnts,, the str array holds the ref. varibale not the values/objects ,, so values can be string  
+>>for (i=0;i<arr.length; i++){
+>>str[i] =in.next();   // user giving the objects values in the terminal 
+  }
+      Sop(Arrays.toString(str));
+
+
+      Sol - // When you give objects 
+        aa, bb, cc, dd   or a, b, c, d  // 4 objects  for diagram in storage in the book 
+        or - str[1]= "Raj", str[2] ="blue ", str[3]= "rocket" etc.... // here the array str holds the ref. varible (stack)i.e str = [ref.var1, ref.var2, 3, 4],, and each ref. var has its value like raj, blue(objects stored in the heap ,, a run time allocation ) etc ,
+    
+  ================================================================================================================================================================
+  ==========================================================================================================================================================
   Problems on array 
 1] revers an given Array - 
   int[] A = new int[1,3,23,9,18];
