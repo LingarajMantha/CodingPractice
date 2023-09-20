71c2982acc38f909294cfbc29b680c55a4fb1366
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-09-20 06:29:30 +0530

    Update Arrays_Problems.java

--- Arrays_Problems.java
+++ Arrays_Problems.java
@@ -18,4 +18,63 @@ Arrays Pronblems ----
     }
 }
 
+ -----------------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-----------------------------------------
+
+   problem 3 - 
+
+27. Remove Element
+Array easy problem leet code 
+
+
+   class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j=0;
+        for (int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                 nums[j] = nums[i]; // for this line even if you dont right the retrun value is orrect but the rray value is differeniung,, so its imp. to write this 
+
+                j++;
+            }
+            
+            
+        }
+        
+  return j;  
+}
+}
+
+--------------------------------------XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-------------------------------------------------
+
+  35. Search Insert Position
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+  class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left=0;
+        int right = nums.length-1;
+        
+        while (left<=right){
+            int mid = (left+right)/2;
+            if (nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return left;
+
+      
+  
+}
+}
+
+
   
