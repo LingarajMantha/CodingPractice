09b7eac7f6aae5d7b497a36e1926e9b798af651e
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-10-04 19:19:11 +0530

    Update and rename LinkedList.java to LinkedListTheory.java

---
+++ LinkedListTheory.java
@@ -0,0 +1,14 @@
+Linked Lists  -- -> 
+important to know how to work with the Head ,, remianing is easy .
+  3-->4-->5-->1-->--18(3 is head and 18 is tail ) where each elemnt is of tyoe Node 
+  ex. - class Node {
+  int val ;
+  Node next;
+
+  }
+
+>> how to initiliaze a linked list - 
+  LinkedList <Integer> list = new LinkedList<>();
+list.add(34); 
+In linked List (LL) which ever varibale has the type Node - is jsut kinda pointer refreing to the the other or address - Link Node Head , tail, Node next ,, these are refering to the Node or 
+  address 
