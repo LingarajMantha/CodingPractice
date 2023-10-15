60b96d337ac4568dbf44d6fdb57b7f0cc24fc280
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-10-16 04:48:52 +0530

    Create String_Theory.java

---
+++ String_Theory.java
@@ -0,0 +1,17 @@
+How to reverse a string - 
+  ex = char[] s = new s[];
+s = {"M","a","n","t","h","a"}    ===> result -> a h t n a M
+
+  class Solution {
+void revrseString (char[] s ){
+  //to reverse a string
+  int left =0;
+  int right = s.length-1;
+  while(left < right){
+    //swaping the array
+    char temp = s[left];
+    s[left]= r[right];
+    s[right]= temp;
+  }
+}
+}
