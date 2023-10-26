a9c75f84c59f7191c59d73273feeeb9252779b8e
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-10-27 05:23:08 +0530

    Update StacksAndQueus.java

--- StacksAndQueus.java
+++ StacksAndQueus.java
@@ -5,4 +5,44 @@ EX -> Plates kept in the stack ,, FILO or LIFO
 **you cant remove the element or plate from the middle ,, default , it will be removed form the top 
 removing -> pop 
 adding or inserting in called - > push 
+Time Complexity - > 
+
+QUEUES - > 
+People standing in the line - > 
+
+
+Stack Data Structure - > 
+ STacks , Queue , graph, maps are the arrays ,, based on their implememtion we dont call them array , but call with individiual name , thats it.
     
+
+
+    -----------------------------------------------------------------------------------------------------------------------------------------------------------
+    QUEUE Data Structure - > 
+    FIFO or LILO- > 
+    Queue is an interface (tell what to do , not how to do . > ), 
+where as stack is Class ,
+
+Implemetation of Queue - > 
+    Queue <Integer> queue = new LinkedList<>();
+to add elemnts in the Queue
+queue.add(3); 
+queue.add(5); 
+queue.add(7); 
+queue.add(32); 
+
+** to remove the elements 
+queue.peek (); ->peek functoion  its not remove , but peek or see who is at the head of the queue, just see but will not remove 
+queue.remove(); removes head of the queue. lly flow follows. 
+
+
+
+
+
+
+
+
+
+
+
+
+
