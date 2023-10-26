2949ba73d3737b38b6f04758ecd5035f15f50556
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2023-10-27 05:07:13 +0530

    Update StacksAndQueus.java

--- StacksAndQueus.java
+++ StacksAndQueus.java
@@ -1,1 +1,8 @@
+STACKS - > 
+FILO or LIFO -> First in last out or last in first out 
+EX -> Plates kept in the stack ,, FILO or LIFO
 
+**you cant remove the element or plate from the middle ,, default , it will be removed form the top 
+removing -> pop 
+adding or inserting in called - > push 
+    
