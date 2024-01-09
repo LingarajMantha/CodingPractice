01eb827513f6770ca889d8df77e9c7f3f8ca608d
LingarajMantha <133241620+LingarajMantha@users.noreply.github.com>
2024-01-09 22:07:15 +0530

    Create TreeQuestions.java

---
+++ TreeQuestions.java
@@ -0,0 +1,6 @@
+problems - no.3  - google - 
+return the suessor the of the given node ,, check book for the diagram 
+code part - we are using the queue for the storing the data , and using the arraylist<> to return the data or in the form of List<>
+
+
+  
